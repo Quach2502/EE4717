ac2f1a5cc40c6aba5dceac74286ab426c4629a43
Long Nguyen <ndlong95@gmail.com>
2017-11-02 16:33:34 +0800

    add hero and slider

--- webapp/home.php
+++ webapp/home.php
@@ -7,6 +7,7 @@
 
 <html>
 <head>
+    <script src="../js/slider.js"></script>
     <style>
         html{
             box-sizing: border-box;
@@ -40,7 +41,126 @@
 
             /*max-width: 1500px;*/
             margin: 20px auto;
+            /*display: flex;*/
+            /*align-items: center;*/
+
             text-align: center;
+        }
+        .hero-wrap{
+
+        }
+
+        .hero-image{
+            background-image: url('../asset/theme.jpg');
+            background-repeat: no-repeat;
+            background-position:center;
+            background-size: cover;
+            text-align: left;
+            /*display: table;*/
+                /*width: 100%;*/
+            height: 100%;
+            position:relative;
+
+        }
+
+        .hero-text{
+            text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: rgb(255, 247, 252);
+
+        }
+
+        .herotext >h1{
+            font-size: 2em;
+        }
+
+        * {box-sizing:border-box}
+
+        /* Slideshow container */
+        .slideshow-container {
+            max-width: 1000px;
+            position: relative;
+            margin: auto;
+        }
+
+        .slider {
+            display: none;
+        }
+
+        .prev, .next{
+            cursor:pointer;
+            position: absolute;
+            top: 50%;
+            width: auto;
+            margin-top:-22px;
+            padding: 16px;
+            color: white;
+            font-weight: bold;
+            font-size: 18px;
+            transition: 0.6s ease;
+            border-radius: 0 3px 3px 0;
+        }
+
+        .next{
+            right: 0;
+            border-radius: 3px 0 0 3px;
+        }
+
+        .prev:hover, .next:hover{
+            background-color: rgba(0,0,0,0.8);
+        }
+
+        .caption {
+            color: #f2f2f2;
+            font-size: 15px;
+            padding: 8px 12px;
+            position: absolute;
+            bottom: 8px;
+            width: 100%;
+            text-align: center;
+        }
+
+        .dot {
+            cursor:pointer;
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #bbb;
+            border-radius: 50%;
+            display: inline-block;
+            transition: background-color 0.6s ease;
+        }
+
+        .active, .dot:hover {
+            background-color: #717171;
+        }
+
+        /* Fading animation */
+        .fade {
+            -webkit-animation-name: fade;
+            -webkit-animation-duration: 1.5s;
+            animation-name: fade;
+            animation-duration: 1.5s;
+        }
+
+        @-webkit-keyframes fade {
+            from {opacity: .4}
+            to {opacity: 1}
+        }
+
+        @keyframes fade {
+            from {opacity: .4}
+            to {opacity: 1}
+        }
+
+
+        .row {
+            margin: auto;
+            clear: both;
+
         }
 
         .five-column{
@@ -54,6 +174,8 @@
 
         .five-column::after{
             clear:both;
+            content: "";
+            display: table;
         }
 
         .img-thumbnail{
@@ -85,9 +207,52 @@
         <a href="#login">Login</a>
         <a href="cart.php">Cart</a>
     </nav>
+
+
 </header>
 <main>
     <div class = "content-wrap">
+        <section id="hero" class="hero-wrap">
+<!--            <img src ="../asset/theme.jpg">-->
+            <div class="hero-image">
+                <div class="hero-text">
+                    <h1>Food247 Service</h1>
+                    <p>Best price online food service</p>
+                    <button>Explore now!</button>
+                </div>
+            </div>
+        </section>
+
+        <section id="slider">
+            <div class = "slideshow-container">
+                <div class = "slider fade">
+                    <img src = "../asset/image1.jpg" style="width: 100%">
+                    <div class ="caption">Caption 1</div>
+                </div>
+
+                <div class = "slider fade">
+                    <img src = "../asset/image2.jpg" style="width: 100%">
+                    <div class ="caption">Caption 2</div>
+                </div>
+
+                <div class = "slider fade">
+                    <img src = "../asset/image3.jpg" style="width: 100%">
+                    <div class ="caption">Caption 3</div>
+                </div>
+
+                <a class="prev" onclick="sliderPlus(-1)">&#10094;</a>
+                <a class="next" onclick="sliderPlus(1)">&#10095;</a>
+            </div>
+
+
+            <div style="text-align:center">
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
+            </div>
+        </section>
+
+        <section id = "food-thumbnail">
         <?php
 
         @$db = new mysqli('localhost', 'root', 11111111, ee4717);
@@ -107,11 +272,15 @@
 //        echo $num_result;
 
         for ($i=0; $i<$num_result; $i++){
+
             $row = $result->fetch_assoc();
             $foodId = $row['foodid'];
             $foodName = $row['name'];
             $imageLink = "../asset/" . $row['imagelink'];
             $foodPrice = $row['price'];
+            if ($i % 5 == 0){
+                echo"<div class='row'>";
+            }
 
             echo "<div class='five-column'> ";
                 echo "<form action='food_info.php' method='post'>";
@@ -124,10 +293,16 @@
                     echo "</div>";
                 echo "</form>";
             echo "</div>";
+
+            if ($i %5 == 4){
+                echo "</div>";
+            }
         }
 
         ?>
+        </section>
     </div>
+
 </main>
 
 </body>
