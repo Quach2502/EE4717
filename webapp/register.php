970668550a646a03491bb326f6b891682079045f
DungQuach <quachdung2502@gmail.com>
2017-11-05 18:02:51 +0800

    fixing

--- webapp/register.php
+++ webapp/register.php
@@ -68,8 +68,35 @@
 		</nav>
 
 		<div class = "content-wrap">
-			<p>This is purchase page</p>
-			
+			<?php
+			include "dbconnect.php";
+			$username = $_POST['username'];
+			$username = mysqli_real_escape_string($db,$username);
+			$fullname = $_POST['fullname'];
+			$fullname = mysqli_real_escape_string($db,$fullname);
+			$address = $_POST['address'];
+			$address = mysqli_real_escape_string($db,$address);
+			$handphone = $_POST['handphone'];
+			$handphone = mysqli_real_escape_string($db,$handphone);
+			$psw = $_POST['psw'];
+			$psw = mysqli_real_escape_string($db,$psw);
+			$email = $_POST['email'];
+			$email = mysqli_real_escape_string($db,$email);
+			$psw = md5($psw);
+			$sql = "INSERT INTO `user`(`username`, `fullname`, `email`, `handphone`, `password`, `address`) VALUES (\"".$username."\",\"".$fullname."\",\"".$email."\",\"".$handphone."\",\"".$psw."\",\"".$address."\")";
+
+			$result = $db->query($sql);
+
+			if (!$result) {
+				echo "There is something wrong now with database. Please try again later!<br>";
+				echo "<a href='home.php'>Go back to home page.</a><br>";
+				echo "<a href='registration.html'>Register again.</a>";
+			}
+			else{
+				echo "Sucessfully.<br>";
+				echo "<a href='home.php'>Go back to home page.</a>";
+			}
+			?>
 		</div>
 	</header>
 </body>
