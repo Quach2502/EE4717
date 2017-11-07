2c3160d509341c653d0a0736ccea1b996f74debb
DungQuach <quachdung2502@gmail.com>
2017-11-07 11:53:29 +0800

    fix

--- webapp/user_info.php
+++ webapp/user_info.php
@@ -181,7 +181,7 @@
 				$orderIdForDetails = $row['orderid'];
 				$sqlDetails = "SELECT * FROM `orderdetails` WHERE `orderid` = '$orderIdForDetails'";
 				$resultDetails = $db->query($sqlDetails);
-				echo '<div id="modal_'.$orderId.'" class="modal">';
+				echo '<div id="modal_'.$orderIdForDetails.'" class="modal">';
 				echo "<span onclick='document.getElementById(\"modal_".$orderId."\").style.display=\"none\"' class=\"close\">×</span>";
 				echo 	'<table class="modal-content animate" border = "1">';
 				echo 		"<thead>";
