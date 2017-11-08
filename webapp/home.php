8d89d18d49de46cdb41b3f5af612ab68c2551ed3
Long Nguyen <ndlong95@gmail.com>
2017-11-08 17:37:30 +0800

    need to fix footer

--- webapp/home.php
+++ webapp/home.php
@@ -13,8 +13,10 @@
     <link rel="stylesheet" href="../css/slider.css">
     <link rel="stylesheet" href="../css/food_thumbnail.css">
     <link rel="stylesheet" href="../css/add_item_quantity.css">
+    <!--    <link rel="stylesheet" href="../css/footer.css">-->
 </head>
 <body>
+
 <!--    Navigation bar -->
 <header id="">
     <?php
@@ -27,6 +29,7 @@
 
 <main>
     <div class = "content-wrap">
+
         <section id="hero" class="hero-wrap">
 <!--            <img src ="../asset/theme.jpg">-->
             <div class="hero-image">
@@ -165,15 +168,104 @@
                     echo "</div>";
                 }
             }
+//test
+        include "functions/food_query.php";
+        for ($i=0; $i<$num_result; $i++){
+            $row = $result->fetch_assoc();
+            $foodId = $row['foodid'];
+            $foodName = $row['name'];
+            $imageLink = "../asset/" . $row['imagelink'];
+            $foodPrice = $row['price'];
+
+            if ($i % 5 == 0){
+                echo"<div class='row'>";
+            }
+
+            echo "<div class='col five-col'> ";
+
+            echo "<div class='food-info'>";
+
+//                            echo "<input type='image' src='".$imageLink."' class='img-thumbnail'>";
+            echo "<span class='badge-info'>30<br><span class = 'label'>mins</span></span>";
+            echo "<span class='tag'>Promoted</span>";
+            echo "<div class = \"img-thumbnail\" style =\"background-image: url('$imageLink');\" onclick = \"tofoodinfo($foodId)\"></div>";
+            echo "<span class='food-name'> Food Name: ".$foodName."</span>";
+            //    echo "<p> Food Id: ".$imageId."</p>";
+
+
+
+            echo '<div class="center">';
+            echo '<div class="input-group">';
+            echo "<span class='food-price'>S$".$foodPrice."</span>";
+            echo '<span class="input-group-btn">';
+            echo '<button id="add-btn-'.$foodId.'" type="button" class="btn btn-success" onclick="add('.$foodId.')">';
+            echo '<span class="glyphicon glyphicon-plus"></span>';
+            echo ' </button>';
+            echo '</span>';
+
+            echo '<span class="input-group-btn">';
+            echo '<button id = "minus-btn-'.$foodId.'" type="button" class="btn btn-danger" onclick="minus('.$foodId.')">';
+            echo '<span class="glyphicon glyphicon-minus"></span>';
+            echo '</button>';
+            echo ' </span>';
+
+            echo '<input id="input-quantity-'.$foodId.'" type="number" class="form-control input-number" placeholder="1" value="1">';
+
+            echo '<span class="input-group-btn">';
+            echo '<button id="addcart-btn-'.$foodId.'" type="button" class="btn btn-danger" 
+                                                            onclick = "addtocart('.$foodId.')">Add to cart</button>';
+            echo '</span>';
+            echo '</div>';
+            echo '</div>';
+            echo "</div>";
+
+            echo "<form id ='foodinfo-form-".$foodId."' action='food_info.php' method='post'>";
+            echo "<input name='foodid' value='".$foodId."' hidden>";
+            echo "</form>";
+
+            echo "<form id = 'addcart-form-".$foodId."' action= 'home.php' method='post' onSubmit='return formValidate()' >";
+            echo '<input name="foodid" hidden value='.$foodId.'>';
+            echo '<input name="foodname" hidden value='.$foodName.'>';
+            echo '<input id="quantity-'.$foodId.'" name="quantity" type="number" value=0 hidden>';
+            echo '<input id="subtotal-'.$foodId.'" name="subtotal" type="number" value=0 hidden>';
+            echo '<input id="foodprice-'.$foodId.'" value='.$foodPrice.' hidden>';
+            echo '<input id ="add_to_cart" name ="add_to_cart" hidden>';
+            echo "</form>";
+
+            echo "</div>";
+
+            if ($i %5 == 4){
+                echo "</div>";
+            }
+        }
 
         ?>
-        </section>
-    </div>
 
+
+        </section>
+
+    </div>
 </main>
+<div class="space"></div>
+
+
 <script src="../js/initHomePage.js"></script>
 <script src="../js/slider.js"></script>
 <script src="../js/searchbar.js"></script>
 <script src="../js/add_item_quantity.js"></script>
+<!---->
+<!--    --><?php
+//    include "templates/footer.php";
+//    ?>
+
 </body>
+
+<footer class="site-footer">
+    <div class="content">
+        Copyright &copy 2017 247Food <br>
+        Contact us: support@247Food.com
+    </div>
+</footer>
+
 </html>
+
---
+++ webapp/templates/footer.php
@@ -0,0 +1,6 @@
+<footer class="site-footer">
+    <div class="content">
+        Copyright &copy 2017 247Food <br>
+        Contact us: support@247Food.com
+    </div>
+</footer>
